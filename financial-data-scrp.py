a2d042ef4ae243ee3987b9da084a863795ef0158
Andeez123 <andeeznutz003@gmail.com>
2025-01-01 14:49:11 +0800

    built top 25 gainers scraper

--- financial-data-scrp.py
+++ financial-data-scrp.py
@@ -7,12 +7,50 @@ from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import time
 
+options = Options()
+options.add_argument("--headless=new")
+
 #web driver setup
 service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=options)
 
 # Navigate to the website
-driver.get('https://finance.yahoo.com/news/')
+driver.get('https://finance.yahoo.com/markets/stocks/gainers/')
 
-while True:
-    pass
+body = driver.find_elements(By.TAG_NAME, 'tr')
+symbols = []
+names = []
+prices = []
+changes = []
+changes_percent = []
+PE_ratios = []
+
+for elem in body:
+    try:
+        symbol = elem.find_element(By.XPATH, "./td[1]").text
+        name = elem.find_element(By.XPATH, "./td[2]").text
+        price = elem.find_element(By.XPATH, "./td[4]").text
+        change = elem.find_element(By.XPATH, "./td[5]").text
+        change_percent = elem.find_element(By.XPATH, "./td[6]").text
+        PE_ratio = elem.find_element(By.XPATH, "./td[10]").text
+
+        symbols.append(symbol)
+        names.append(name)
+        prices.append(price)
+        changes.append(change)
+        changes_percent.append(change_percent)
+        PE_ratios.append(PE_ratio)
+
+    except:
+        pass
+df = pd.DataFrame({
+    'Symbol': symbols,
+    'Name': names,
+    'Price': prices,
+    'Change': changes,
+    'Change_percent': changes_percent,
+    'PE_Ratio': PE_ratios
+})
+df.to_csv('Top_25_Gainers.csv', index=False)
+
+driver.quit()
