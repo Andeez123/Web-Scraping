09e04a1a53388f97b2ed1f1d62aef87c8330a788
Andeez123 <andeeznutz003@gmail.com>
2025-01-01 16:23:35 +0800

    removed unused libraries

--- financial-data-scrp.py
+++ financial-data-scrp.py
@@ -2,10 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
-import time
 
 options = Options()
 options.add_argument("--headless=new")
